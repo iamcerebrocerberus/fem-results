27d22a300162ae7509eecf7b06c3b4ff86e37eb5
iamcerebrocerberus <sethamek@gmail.com>
2023-07-17 00:44:57 +0000

    added files and assets

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      lightred: "hsl(0, 100%, 67%)",
+      orangeyyellow: "hsl(39, 100%, 56%)",
+      greenteal: "hsl(166, 100%, 37%)",
+      cobaltblue: "hsl(234, 85%, 45%)",
+      lightslateblue: "hsl(252, 100%, 67%)",
+      lightroyalblue: "hsla(241, 81%, 54%)",
+      violetblue: "hsla(256, 72%, 46%, 1)",
+      persianblue: "hsla(241, 72%, 46%, 0)",
+      white: "hsl(0, 0%, 100%)",
+      paleblue: "hsl(221, 100%, 96%)",
+      lightlavender: "hsl(241, 100%, 89%)",
+      darkgrayblue: "hsl(224, 30%, 27%)",
+    },
+    fontFamily: {
+      sans: ["Hanken Grotesk", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
